08795b744d7ca3348a68ed2369f54831779ff7a6
YEON Jewook <kihotemper554@gmail.com>
2025-04-07 18:45:08 +0900

    Create BMI

---
+++ jw/BMIdemo/BMIdemo/BMIdemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  BMIdemoApp.swift
+//  BMIdemo
+//
+//  Created by 제욱 on 4/7/25.
+//
+
+import SwiftUI
+
+@main
+struct BMIdemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ jw/BMIdemo/BMIdemoTests/BMIdemoTests.swift
@@ -0,0 +1,17 @@
+//
+//  BMIdemoTests.swift
+//  BMIdemoTests
+//
+//  Created by 제욱 on 4/7/25.
+//
+
+import Testing
+@testable import BMIdemo
+
+struct BMIdemoTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
